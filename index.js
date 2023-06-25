abaec81c69a4c5201d1a2782cf78fc0f49750bad
Lisa Ryd-Carlsson <117908955+LisaRydCarlsson@users.noreply.github.com>
2023-06-25 08:08:41 +0200

    Updated file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 
-const beansRoutes = require("./routes/beansRoutes.js");
-const userRoutes = require("./routes/userRoutes.js");
 const menuRoutes = require("./routes/menuRoutes.js");
+const orderRoutes = require("./routes/orderRoutes.js");
+const userRoutes = require("./routes/userRoutes.js");
 
 const app = express();
 const PORT = 1337;
 
 app.use(express.json());
-app.use(beansRoutes);
-app.use(userRoutes);
 app.use(menuRoutes);
+app.use(orderRoutes);
+app.use(userRoutes);
 
 app.listen(PORT, () => {
    console.log("Listening on port", PORT);
